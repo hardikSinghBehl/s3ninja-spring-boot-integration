c6f0863842766545424a6d75442d3481104a6086
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-07-05 18:15:55 +0530

    project initialization

---
+++ src/main/java/com/behl/emulator/S3ninjaSpringBootIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.behl.emulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ninjaSpringBootIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ninjaSpringBootIntegrationApplication.class, args);
+	}
+
+}
